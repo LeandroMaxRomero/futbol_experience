27b75baef6a0154b1a2498a846efab35cfeb9538
Dariel Cano <dariel.cano1992@gmail.com>
2023-08-04 08:58:58 -0600

    agregado codigo de mostrar mensaje de bienvenida

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,8 +1,54 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { LenguaContext } from "../../context/LenguaProvider";
+import { useGeolocated } from "react-geolocated";
+import { toast } from "react-toastify";
 
 export const HomePage = () => {
   const { lang } = useContext(LenguaContext);
+
+  /* Agregando funcion para mostrar pais desde donde se hace la consulta 
+  a la pagina.
+   */
+
+  /* key del openwheater(despues se la enmascaramos) */
+  let key = "77894d358b535f22b04a3809825e5b62";
+
+  /* estado de dependendcia instalada useGeolocated */
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+    useGeolocated({
+      positionOptions: {
+        enableHighAccuracy: false,
+      },
+    });
+
+  /* funcion para obtener el pais a partir de iso de paises */
+  function getCountryName(countryCode) {
+    fetch("./paises.json")
+      .then((res) => res.json())
+      .then((data) => {
+        if (countryCode !== "") {
+          console.log(data[countryCode]);
+          toast(`Gracias por visitarnos desde ${data[countryCode]}`);
+        }
+      });
+  }
+
+  /* al cargar la pagina muestra el pais*/
+  useEffect(() => {
+    if (isGeolocationAvailable) {
+      if (isGeolocationEnabled) {
+        if (coords) {
+          fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${key}`
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              getCountryName(data.sys.country);
+            });
+        }
+      }
+    }
+  }, [coords]);
 
   return (
     <div>
