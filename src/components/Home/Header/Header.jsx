f67ad00e4016f92973ec438094ef23a40dea60b0
Leandro Romero <lea.max.rom@gmail.com>
2023-08-15 04:04:59 -0300

    0.5.1

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -2,6 +2,7 @@
 // import { LenguaContext } from "../../context/LenguaProvider";
 import "../../../scss/layout/_header.scss";
 import { NavBar } from "../../NavBar/NavBar";
+// import { Carrusel } from "./Carrusel";
 import { Legend } from "./Legend";
 import { SocialBox } from "./SocialBox";
 
@@ -10,12 +11,14 @@ export const Header = () => {
   return (
     <div className="container-header">
 
-      <img src="img/boca-juniors-hero.png" alt="Hinchada Boca Juniors" className="imagen-fondo-header"/>
+      <img src="img/hero1.png" alt="Hinchada Boca Juniors" className="imagen-fondo-header"/>
+      
+      {/* <Carrusel/> */}
 
       <div className="header">
         <NavBar />
-        <SocialBox />
         <Legend />
+        <SocialBox />
       </div>
       
     </div>
