b17a0abb79c2b0aa2fac99fa72ea125f8e6fd87b
Dariel Cano <dariel.cano1992@gmail.com>
2023-08-16 18:19:00 -0600

    cambios en imagenes de slide vista mobile

--- src/components/Home/Header/ImageSlider.jsx
+++ src/components/Home/Header/ImageSlider.jsx
@@ -1,46 +1,48 @@
-
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
-import 'animate.css';
+import "animate.css";
 
+export const ImageSlider = ({ slides }) => {
+  const [currentIndex, setCurretIndex] = useState(0);
 
-export const ImageSlider = ({slides}) => {
-    const [currentIndex, setCurretIndex] = useState(0);
+  // const goToPrevious = () => {
+  //     const isFirstSlide = currentIndex === 0;
+  //     const newIndex = isFirstSlide ? slides.length -1 : currentIndex -1;
+  //     setCurretIndex(newIndex);
+  // }
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurretIndex(newIndex);
+  };
+  // const goToSlide = slideIndex => {
+  //     setCurretIndex(slideIndex);
+  // }
 
-    // const goToPrevious = () => {
-    //     const isFirstSlide = currentIndex === 0;
-    //     const newIndex = isFirstSlide ? slides.length -1 : currentIndex -1;
-    //     setCurretIndex(newIndex);
-    // }
-    const goToNext = () => {
-        const isLastSlide = currentIndex === slides.length -1;
-        const newIndex = isLastSlide ? 0 : currentIndex +1;
-        setCurretIndex(newIndex);
-    }
-    // const goToSlide = slideIndex => {
-    //     setCurretIndex(slideIndex);
-    // }
-
-    useEffect(() => {
-        setInterval(() => {
-            goToNext();
-          },3000);
-    },);
+  useEffect(() => {
+    setInterval(() => {
+      goToNext();
+    }, 3000);
+  });
 
   return (
     <>
-        {/* <div className="arrow left" onClick={goToPrevious}>&#10094;</div> */}
+      {/* <div className="arrow left" onClick={goToPrevious}>&#10094;</div> */}
 
-        <img src={slides[currentIndex].url} alt="Imagen Header" className={"animate__animated animate__infinite imagen-fondo-header"} />
+      <img
+        src={slides[currentIndex].url}
+        alt="Imagen Header"
+        className={"animate__animated animate__infinite imagen-fondo-header"}
+      />
 
-        {/* <div className="arrow right" onClick={goToNext}>&#10095;</div> */}
+      {/* <div className="arrow right" onClick={goToNext}>&#10095;</div> */}
 
-        {/* <div className="contenedor-puntos">{slides.map((slide, slideIndex)=> (
+      {/* <div className="contenedor-puntos">{slides.map((slide, slideIndex)=> (
             <div key={slideIndex} className="punto-carrusel" onClick={()=>goToSlide(slideIndex)}>&#10625;</div>
         ))}</div> */}
     </>
-  )
+  );
 };
 ImageSlider.propTypes = {
-    slides: PropTypes.node.isRequired,
-}
+  slides: PropTypes.node.isRequired,
+};
