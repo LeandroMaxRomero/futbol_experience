33a7328f279560ebeb7a6b770b55bc9dca795a80
Dariel Cano <dariel.cano1992@gmail.com>
2023-08-18 15:08:01 -0600

    cambios en nosotros, falta aun

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -1,16 +1,20 @@
 import { HeaderContact } from "../Contact/HeaderContact";
 import { AcercaDe } from "./AcercaDe";
 import { DetrasDe } from "./DetrasDe";
-import '../../scss/layout/_nosotros.scss'
+import "../../scss/layout/_nosotros.scss";
 import { ReservaNosotros } from "./ReservaNosotros";
 
 export const Nosotros = () => {
   return (
     <div className="nosotros">
-      <HeaderContact imagen={"img/header-nosotros.png"} tittleES="Nosotros" tittleEN="About Us"/>
-      <AcercaDe/>
-      <DetrasDe/>
-      <ReservaNosotros/>
+      <HeaderContact
+        imagen={"img/header-nosotros.png"}
+        tittleES="Nosotros"
+        tittleEN="About Us"
+      />
+      <AcercaDe />
+      <DetrasDe />
+      <ReservaNosotros />
     </div>
-  )
+  );
 };
